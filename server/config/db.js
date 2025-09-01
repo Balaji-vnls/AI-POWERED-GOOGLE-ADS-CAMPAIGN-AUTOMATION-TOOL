9153f4b8cea7a65871955ad5229ea8e21c9d09f9
Balaji-vnls <balajinarayan1964@gmail.com>
2025-09-01 23:12:25 +0530

    Configured MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,13 @@
-// server/config/db.js
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://localhost:27017/ad_campaign_db',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log('MongoDB connected.');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected');
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
